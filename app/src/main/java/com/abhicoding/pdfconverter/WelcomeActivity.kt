d32ff5f88ea964001fda5f0d1ecb6573ac866fea
abhinandan-verma <abhinandanverma551@gmail.com>
2024-01-15 22:15:49 +0530

    Initial commit

---
+++ app/src/main/java/com/abhicoding/pdfconverter/WelcomeActivity.kt
@@ -0,0 +1,17 @@
+package com.abhicoding.pdfconverter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.abhicoding.pdfconverter.ui.theme.PDFConverterTheme
+
+class WelcomeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PDFConverterTheme {
+
+            }
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+@Suppress("UnstableApiUsage")
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PDF Converter"
+include(":app")
+
+@Suppress("UnstableApiUsage")
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
